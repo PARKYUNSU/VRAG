21866d7808a91f23cd34109c113424b7a66cdb16
PARKYUNSU <125172299+PARKYUNSU@users.noreply.github.com>
2025-03-21 12:31:25 +0900

    commit

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ brew install --cask google-cloud-sdk
 ```terminal
 gsutil cp -R gs://gresearch/magiclens/models ./
 ```
+
+```terminal
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+```
+
+```terminal
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+```
--- cfg.py
+++ cfg.py
@@ -4,7 +4,7 @@ class Config:
         self.imgs_per_category = 300
         self.milvus_uri = "milvus.db"
         self.collection_name = "cir_demo_large"
-        self.device = "mps"
+        self.device = "cpu"
         self.model_type = "large"
         self.model_path = "./models/magic_lens_clip_large.pkl"
         self.api_type = "openai"
